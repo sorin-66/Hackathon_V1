2c620e42561a1997fd6b7ce8f7ef5d5543b90ba0
sorin-66 <74119619+sorin-66@users.noreply.github.com>
2021-02-08 03:51:36 +0000

    Update Hackathon_13.py

--- Hackathon_13.py
+++ Hackathon_13.py
@@ -17,18 +17,31 @@ def snap():
 
     player_1 = []
     player_2 = []
+    score_1 = 0
+    score_2 = 0
     # deal the cards to the players
-    for i in range(len(cards)-1):
-        player_1.append(cards[i])
-        player_2.append(cards[i+1])
+    for i in range(26):
+        player_1.append(cards[2*i])
+        player_2.append(cards[2*i+1])
         
     for i in range(26):
         print("Player 1  |   Player 2 ")
         print(f"    {player_1[i]}     |       {player_2[i]}")
         if player_1[i] == player_2[i]:
             print('Snap!')
+            if score_1 > score_2:
+                print(f" Player 1 win with {score_1} - {score_2}")
+            elif score_1 < score_2:
+                print(f" Player 2 win with {score_2} - {score_1}")
+            else:
+                print(f" Snap-Snap! nobody wins equal score: {score_1}")
             return
         else:
+            if player_1[i] > player_2[i]:
+                score_1 +=1
+            else:
+                score_2 +=1
             input("Press enter to continue:")
     
 snap()                  
+
